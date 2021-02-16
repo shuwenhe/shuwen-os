c7a21d22c4661f9fa2512eb6b7faeb7923f03534
ShuwenHe <1201220707@pku.edu.cn>
2021-02-16 15:04:18 +0800

    fix: echoarg

--- test/echoarg/echoarg.c
+++ test/echoarg/echoarg.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-for(i = 0; i < argc; i++)
-printf("argv[%d]:%s\n",i,argv[i]);
-exit(0);
+    int i;
+    for (i = 0; i < argc; i++)
+        printf("argv[%d]:%s\n", i, argv[i]);
+    exit(0);
 }
