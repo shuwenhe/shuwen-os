df0806f918f723b63b7656c3c11a08493a7bf1a7
ShuwenHe <1201220707@pku.edu.cn>
2021-03-16 02:08:53 +0800

    feat: os

---
+++ posix/stdio.h/getchar/getchar.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char *menu[] = {
+    "a - add new record",
+    "d -delete record",
+    "q -quit",
+    NULL,
+};
+
+int getchoice(char *greet,char *choices[]);
+
+int main(){
+    int choice = 0;
+
+    if (!isatty(fileno(stdout))){ //isatty标准输出是否已连接到一个终端，如果没有退出程序 
+        fprintf(stderr,"You are not a terminal!\n");
+        exit(1);
+    }
+    
+    do {
+        choice = getchoice("Please select an action",menu);
+        printf("You have chosen:%c\n",choice);
+    }while (choice != 'q');
+    exit(0);
+}
+
+int getchoice(char *greet, char *choices[]){
+    int chosen = 0;
+    int selected;
+    char **option;
+
+    do {
+        printf("Choice:%s\n",greet);
+        option = choices;
+        while (*option){
+            printf("%s\n",*option);
+            option++;
+        }
+        selected = getchar();
+        option = choices;
+        while (*option){
+            if (selected == *option[0]){
+                chosen = 1;
+                break;
+            }
+            option++;
+        }
+        if (!chosen){
+            printf("Incorrect choice,select again\n");
+        }
+    }while (selected == '\n');
+    return selected;
+}
