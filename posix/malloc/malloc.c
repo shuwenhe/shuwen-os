13cc4b80039ff40471849e936347ccf16d293469
ShuwenHe <1201220707@pku.edu.cn>
2021-03-16 17:13:59 +0800

    feat: malloc

--- posix/malloc/malloc.c
+++ posix/malloc/malloc.c
@@ -1,7 +1,19 @@
-#include "malloc.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-//	brk();
-	printf("unistd brk\n");
+#define A_MEGABYTE (1024*1024)
+
+int main(){
+	char *some_memory;
+	int megabyte = A_MEGABYTE;
+	int exit_code = EXIT_FAILURE;
+
+    some_memory = (char *)malloc(megabyte); // malloc函数返回值是一个void *指针，需要类型转换成我们需要的char *类型
+	if (some_memory != NULL){
+		sprintf(some_memory,"Hello world\n");
+		printf("%s",some_memory);
+		exit_code = EXIT_SUCCESS;
+	}
+	exit(exit_code);
 }
